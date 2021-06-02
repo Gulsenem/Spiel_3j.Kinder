2372d8a83f2c4d6049ad206b2ac56957dfcc030c
Gulsenem <og.durdyeva@gmail.com>
2021-06-03 00:46:28 +0200

    ***

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-
+var x = document.getElementById("box1");
 
 function kliked(farbe)
 {
     
+    if( farbe == "red")
+    {
+        x.style.backgroundColor = "red";
+    }
+    else if (farbe == "blue")
+    {
+        x.style.backgroundColor = "blue";
+    }
+    else if (farbe == "green")
+    {
+        x.style.backgroundColor = "green";
+    }
+    else
+    {
+        x.style.backgroundColor = "hsl(258, 74%, 62%)";
+    }
 }
