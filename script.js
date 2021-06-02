a27dbdf9c73dc5a7b2375f7f75569f78f789dd56
Gulsenem <og.durdyeva@gmail.com>
2021-06-03 01:07:25 +0200

    ///

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-var x = document.getElementById("box1");
 
-function kliked(farbe)
+for (var i=0; i<2; i++)
 {
-    
-    if( farbe == "red")
+    var box1 = document.createElement("div");
+    document.getElementById("box").appendChild(box1);
+    var x = document.getElementById("box1");
+
+    function kliked(farbe)
     {
-        x.style.backgroundColor = "red";
-    }
-    else if (farbe == "blue")
-    {
-        x.style.backgroundColor = "blue";
-    }
-    else if (farbe == "green")
-    {
-        x.style.backgroundColor = "green";
-    }
-    else
-    {
-        x.style.backgroundColor = "hsl(258, 74%, 62%)";
+
+        if( farbe == "red")
+        {
+            box1.style.backgroundColor = "red";
+        }
+        else if (farbe == "blue")
+        {
+            box1.style.backgroundColor = "blue";
+        }
+        else if (farbe == "green")
+        {
+            box1.style.backgroundColor = "green";
+        }
+        else
+        {
+            box1.style.backgroundColor = "hsl(258, 74%, 62%)";
+        }
     }
 }
